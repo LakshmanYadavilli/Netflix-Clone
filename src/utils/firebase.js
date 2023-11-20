a2d05e7732bb8a9b537605e9acb796b2f40c1fec
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2023-11-20 16:39:42 +0530

    Api key Changed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,28 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD8t95BtAdNbZqSOZrpoli218UA4WoqoGY",
+//   authDomain: "netflix-clone-817e1.firebaseapp.com",
+//   projectId: "netflix-clone-817e1",
+//   storageBucket: "netflix-clone-817e1.appspot.com",
+//   messagingSenderId: "154719228304",
+//   appId: "1:154719228304:web:e7c5815c08dadc73ee2315",
+//   measurementId: "G-MX48ZBR4GK",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export const auth = getAuth();
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -8,17 +33,16 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBuMG1ZeO5WPfZ_AAtOk6iGE00ycLCnwA",
-  authDomain: "netflix-clone-5b1cf.firebaseapp.com",
-  projectId: "netflix-clone-5b1cf",
-  storageBucket: "netflix-clone-5b1cf.appspot.com",
-  messagingSenderId: "1033779598680",
-  appId: "1:1033779598680:web:367395bdc4ddf5c636412d",
-  measurementId: "G-S3FL4N4LW4",
+  apiKey: "AIzaSyD8t95BtAdNbZqSOZrpoli218UA4WoqoGY",
+  authDomain: "netflix-clone-817e1.firebaseapp.com",
+  projectId: "netflix-clone-817e1",
+  storageBucket: "netflix-clone-817e1.appspot.com",
+  messagingSenderId: "154719228304",
+  appId: "1:154719228304:web:e7c5815c08dadc73ee2315",
+  measurementId: "G-MX48ZBR4GK",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 export const auth = getAuth();
